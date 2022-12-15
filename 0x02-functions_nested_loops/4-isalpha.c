08c4aaede1d870732d338dfd8c233e451e276cc9
QwejoD <desmondqwejo@gmail.com>
2022-12-14 18:47:11 -0800

    task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,14 +13,3 @@ void test_isalpha(int n)
 	_putchar(r + '0');
 	_putchar('\n');
 }
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	test_isalpha('H');
-	return (0);
-}
